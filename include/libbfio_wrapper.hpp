a1ff9fb6bc5d3e913e749fbfde05f2b949092fb5
sja <sja@digital-forensic.org>
2016-02-18 11:41:35 +0100

    use namespace dff in libbfio wrapper

--- include/libbfio_wrapper.hpp
+++ include/libbfio_wrapper.hpp
@@ -17,10 +17,15 @@
 #ifndef __LIBBFIO_WRAPPER_HPP__
 #define __LIBBFIO_WRAPPER_HPP__
 
+#include "export.hpp"
+
 #include "stdint.h"
 #include "libbfio.h"
-#include "node.hpp"
-#include "vfile.hpp"
+
+namespace DFF
+{
+class Node;
+class VFile;
 
 struct dff_libbfio_file_io_handle
 {
@@ -36,11 +41,13 @@ EXPORT int	dff_libbfio_file_io_handle_free(intptr_t **io_handle, libbfio_error_t
 EXPORT int 	dff_libbfio_file_io_handle_clone(intptr_t **destination_io_handle, intptr_t *source_io_handle, libbfio_error_t **error);
 EXPORT int 	dff_libbfio_file_open(intptr_t *io_handle, int access_flags, libbfio_error_t **error);
 EXPORT int 	dff_libbfio_file_close(intptr_t *io_handle, libbfio_error_t **error);
-EXPORT ssize_t dff_libbfio_file_read(intptr_t *io_handle, uint8_t *buffer, size_t size, libbfio_error_t **error);
-EXPORT ssize_t dff_libbfio_file_write(intptr_t *io_handle, const uint8_t *buffer, size_t size, libbfio_error_t **error);
-EXPORT off64_t dff_libbfio_file_seek_offset(intptr_t *io_handle, off64_t offset, int whence, libbfio_error_t **error);
+EXPORT ssize_t  dff_libbfio_file_read(intptr_t *io_handle, uint8_t *buffer, size_t size, libbfio_error_t **error);
+EXPORT ssize_t  dff_libbfio_file_write(intptr_t *io_handle, const uint8_t *buffer, size_t size, libbfio_error_t **error);
+EXPORT off64_t  dff_libbfio_file_seek_offset(intptr_t *io_handle, off64_t offset, int whence, libbfio_error_t **error);
 EXPORT int 	dff_libbfio_file_exists(intptr_t *io_handle, libbfio_error_t **error);
 EXPORT int 	dff_libbfio_file_is_open(intptr_t *io_handle, libbfio_error_t **error);
 EXPORT int 	dff_libbfio_file_get_size(intptr_t *io_handle, size64_t *size, libbfio_error_t **error);
+
+}
 
 #endif
