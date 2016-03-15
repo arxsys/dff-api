2e9a65d19272d2b52d91e6f5d0d56050f0a7db05
fba <fba@arxsys.fr>
2016-03-15 13:47:29 +0100

    * Remove bad typedef used when targeting Windows 32bits

--- magic/file.h
+++ magic/file.h
@@ -43,7 +43,7 @@
     #ifdef  _WIN64
      typedef signed __int64    ssize_t;
     #else
-     typedef _W64 signed int   ssize_t;
+     typedef signed int   ssize_t;
     #endif
     #define _SSIZE_T_DEFINED
   #endif
